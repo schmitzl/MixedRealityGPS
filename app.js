5a1429b8c5c763329d8de1eb3fbce4eeb7554680
schmitzl <lisa.schmitz66@googlemail.com>
2017-03-20 19:11:27 +0100

    calculate angle

--- app.js
+++ app.js
@@ -212,7 +212,7 @@ app.updateEvent.addEventListener(function (frame) {
     a.applyMatrix3( camera.matrixWorld );
     var b = new THREE.Vector3(a.x + camDir.x, a.y + camDir.y, a.z + camDir.z);
     b.sub(a);
-    var c = boxPose.position.clone();
+    var c = new Vector3(boxPose.position.x, boxPose.position.y, boxPose.position.z);
     c.sub(a);
     b.cross(v);
 
@@ -228,7 +228,7 @@ app.updateEvent.addEventListener(function (frame) {
 
     
 
-    var infoText = 'cross2:<br>';
+    var infoText = 'cross3:<br>';
     infoText += 'Your location is lla (' + toFixed(gpsCartographicDeg[0], 6) + ', ';
     infoText += toFixed(gpsCartographicDeg[1], 6) + ', ' + toFixed(gpsCartographicDeg[2], 2) + ')';
     infoText += 'dot + ' + b.z;
