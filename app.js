5324379d134ad2909954bef999ccc2d60d69478f
schmitzl <lisa.schmitz66@googlemail.com>
2017-04-08 13:07:29 +0200

    Fix: show arrows every time the user is searching for an object

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ var stadshuset = new THREE.Object3D();
 loadTramScene();
 tramScene.rotation.y = Math.PI;
 tramScene.translateX(-1);
-var tramSceneGeoPos = Cartesian3.fromDegrees(18.071775, 59351258, 18.13);                    //(17.920747, 59.374212, 11.97);
+var tramSceneGeoPos = Cartesian3.fromDegrees(18.071775, 59351258, 23.13);                    //(17.920747, 59.374212, 11.97);
 var tramSceneGeoEntity = new Argon.Cesium.Entity({
     position: new Cesium.ConstantPositionProperty(tramSceneGeoPos, ReferenceFrame.FIXED),
     orientation: Cesium.Quaternion.IDENTITY
@@ -105,7 +105,7 @@ var graffitiTram = new THREE.Object3D();
 var graffitiMaskingPlane = new THREE.Object3D();
 loadgraffitiScene();
 graffitiTramScene.scale.set(0.25, 0.35, 0.25);
-var graffitiTramSceneGeoPos = Cartesian3.fromDegrees(18.071775, 59351258, 18.13);            //(17.920747, 59.374212, 11.97);
+var graffitiTramSceneGeoPos = Cartesian3.fromDegrees(18.071775, 59351258, 23.13);            //(17.920747, 59.374212, 11.97);
 var graffitiTramSceneGeoEntity = new Argon.Cesium.Entity({
     position: new Cesium.ConstantPositionProperty(graffitiTramSceneGeoPos, ReferenceFrame.FIXED),
     orientation: Cesium.Quaternion.IDENTITY
@@ -116,7 +116,7 @@ var schedule = new THREE.Object3D();
 var schedulePost = new THREE.Object3D();
 var scheduleBox = new THREE.Object3D();
 loadSchedule();
-var scheduleGeoPos = Cartesian3.fromDegrees(18.071775, 59351258, 18.13);                     //(17.920747, 59.374212, 11.97);
+var scheduleGeoPos = Cartesian3.fromDegrees(18.071775, 59351258, 23.13);                     //(17.920747, 59.374212, 11.97);
 var scheduleGeoEntity = new Argon.Cesium.Entity({
     position: new Cesium.ConstantPositionProperty(scheduleGeoPos, ReferenceFrame.FIXED),
     orientation: Cesium.Quaternion.IDENTITY
